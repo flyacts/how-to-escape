eef3ff25dca1f71623ee1238ac4643ab5f30795b
Mike Konczak <mike.konczak@flyacts.com>
2023-05-04 14:50:17 +0200

    feat: input code on qs keyboard

---
+++ src/app/helpers/create-rectangle.function.ts
@@ -0,0 +1,43 @@
+/*!
+ * @copyright FLYACTS GmbH 2022
+ */
+
+import { Graphics, Point } from 'pixi.js';
+
+interface RectangleData {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    color: number;
+    angle?: number;
+    alpha?: number;
+}
+
+export class Rectangle extends Graphics {
+    public data!: RectangleData;
+}
+
+/**
+ * create arrow
+ */
+export function createRectangle(
+    data: RectangleData,
+): Rectangle {
+    const { x, y, width, height, color, angle, alpha } = data;
+    const rectangle = new Rectangle();
+
+    rectangle.lineStyle(5, 0xEEEEEE);
+    rectangle.beginFill(color);
+    rectangle.angle = angle ?? 0;
+    rectangle.drawRect(x, y, width, height);
+    rectangle.endFill();
+
+    rectangle.alpha = alpha ?? 1;
+    rectangle.interactive = true;
+    rectangle.cursor = 'pointer';
+
+    rectangle.data = data;
+
+    return rectangle;
+}
--- src/app/templates/desk-qs-keyboard/desk-qs-keyboard.component.ts
+++ src/app/templates/desk-qs-keyboard/desk-qs-keyboard.component.ts
@@ -7,6 +7,18 @@ import { Component, computed, Signal } from '@angular/core';
 import { Scene } from '../../enum';
 import { Arrow, createArrow } from '../../helpers';
 import { SceneService } from '../../services/scene.service';
+import { Rectangle, createRectangle } from '../../helpers/create-rectangle.function';
+
+const KEYS: Map<string, { x: number, y: number }> = new Map();
+
+KEYS.set('c', { x: 384, y: 740 });
+KEYS.set('r', { x: 396, y: 624 });
+KEYS.set('4', { x: 370, y: 566 });
+KEYS.set('f', { x: 410, y: 682 });
+KEYS.set('t', { x: 454, y: 626 });
+KEYS.set('w', { x: 280, y: 624 });
+KEYS.set('3', { x: 308, y: 566 });
+KEYS.set('k', { x: 638, y: 686 });
 
 @Component({
     selector: 'app-desk-qs-keyboard',
@@ -16,6 +28,8 @@ import { SceneService } from '../../services/scene.service';
 export class DeskQsKeyboardComponent {
 
     public iconSrc: Signal<string>;
+
+    public text = '';
 
     public constructor(
         private sceneService: SceneService,
@@ -28,6 +42,55 @@ export class DeskQsKeyboardComponent {
         const arrow = this.createDeskQsArrow();
 
         this.sceneService.pixiApp?.stage.addChild(arrow);
+
+        for (const key of KEYS.entries()) {
+            const keyRect = this.createKeyRect(key[1].x, key[1].y);
+
+            keyRect.on('mouseup', () => {
+                console.log(`pressed ${key[0]}`);
+
+                this.text = `${this.text}${key[0]}`;
+                this.text = this.text.substring(0, 9);
+            });
+
+            this.sceneService.pixiApp?.stage.addChild(keyRect);
+        }
+
+        const keyEnterRect = createRectangle({
+            x: 910,
+            y: 630,
+            color: 0x212121,
+            width: 64,
+            height: 96,
+            angle: 0,
+            alpha: 0,
+        });
+
+        keyEnterRect.on('mouseup', () => {
+            console.log(`pressed enter`);
+
+            this.text = '';
+        });
+
+        this.sceneService.pixiApp?.stage.addChild(keyEnterRect);
+
+        const keyBackspaceRect = createRectangle({
+            x: 880,
+            y: 570,
+            color: 0x212121,
+            width: 106,
+            height: 42,
+            angle: 0,
+            alpha: 0,
+        });
+
+        keyBackspaceRect.on('mouseup', () => {
+            console.log(`pressed backspace`);
+
+            this.text = this.text.slice(0, -1);
+        });
+
+        this.sceneService.pixiApp?.stage.addChild(keyBackspaceRect);
     }
 
     /**
@@ -48,5 +111,22 @@ export class DeskQsKeyboardComponent {
         };
 
         return deskQs;
+    }
+
+    /**
+     * create arrow to deskQs
+     */
+    public createKeyRect(x: number, y: number): Rectangle {
+        const keyRect = createRectangle({
+            x,
+            y,
+            color: 0x212121,
+            width: 32,
+            height: 32,
+            angle: 0,
+            alpha: 0,
+        });
+
+        return keyRect;
     }
 }
