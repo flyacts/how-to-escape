d4207742201c03108ef3575b9cfaf6d3e87b68c2
msc <mirko.schulze@flyacts.com>
2023-11-09 11:52:25 +0100

    feat: add more keys to qsKeyboard

--- src/app/templates/desk-qs-keyboard/desk-qs-keyboard.component.ts
+++ src/app/templates/desk-qs-keyboard/desk-qs-keyboard.component.ts
@@ -13,14 +13,58 @@ import { TextService } from '../../services/text.service';
 
 const KEYS: Map<string, { x: number, y: number }> = new Map();
 
-KEYS.set('c', { x: 384, y: 740 });
-KEYS.set('r', { x: 396, y: 624 });
-KEYS.set('4', { x: 370, y: 566 });
-KEYS.set('f', { x: 410, y: 682 });
-KEYS.set('t', { x: 454, y: 626 });
-KEYS.set('w', { x: 280, y: 624 });
-KEYS.set('3', { x: 308, y: 566 });
-KEYS.set('k', { x: 638, y: 686 });
+// set keyboard coordinates
+KEYS.set('^', { x: 132, y: 560 });
+KEYS.set('1', { x: 192, y: 560 });
+KEYS.set('2', { x: 250, y: 562 });
+KEYS.set('3', { x: 304, y: 562 });
+KEYS.set('4', { x: 366, y: 564 });
+KEYS.set('5', { x: 424, y: 564 });
+KEYS.set('6', { x: 480, y: 566 });
+KEYS.set('7', { x: 538, y: 566 });
+KEYS.set('8', { x: 592, y: 566 });
+KEYS.set('9', { x: 652, y: 568 });
+KEYS.set('0', { x: 708, y: 568 });
+KEYS.set('ß', { x: 768, y: 570 });
+KEYS.set('´', { x: 824, y: 570 });
+
+KEYS.set('q', { x: 222, y: 620 });
+KEYS.set('w', { x: 280, y: 620 });
+KEYS.set('e', { x: 336, y: 622 });
+KEYS.set('r', { x: 394, y: 622 });
+KEYS.set('t', { x: 450, y: 624 });
+KEYS.set('z', { x: 508, y: 624 });
+KEYS.set('u', { x: 566, y: 626 });
+KEYS.set('i', { x: 624, y: 626 });
+KEYS.set('o', { x: 682, y: 628 });
+KEYS.set('p', { x: 740, y: 628 });
+KEYS.set('ü', { x: 796, y: 630 });
+KEYS.set('+', { x: 854, y: 630 });
+
+KEYS.set('a', { x: 238, y: 676 });
+KEYS.set('s', { x: 294, y: 676 });
+KEYS.set('d', { x: 352, y: 678 });
+KEYS.set('f', { x: 410, y: 678 });
+KEYS.set('g', { x: 466, y: 678 });
+KEYS.set('h', { x: 524, y: 680 });
+KEYS.set('j', { x: 580, y: 680 });
+KEYS.set('k', { x: 638, y: 680 });
+KEYS.set('l', { x: 694, y: 682 });
+KEYS.set('ö', { x: 754, y: 682 });
+KEYS.set('ä', { x: 810, y: 684 });
+KEYS.set('#', { x: 868, y: 684 });
+
+KEYS.set('<', { x: 210, y: 732 });
+KEYS.set('y', { x: 266, y: 732 });
+KEYS.set('x', { x: 324, y: 734 });
+KEYS.set('c', { x: 380, y: 734 });
+KEYS.set('v', { x: 440, y: 736 });
+KEYS.set('b', { x: 494, y: 738 });
+KEYS.set('n', { x: 550, y: 738 });
+KEYS.set('m', { x: 610, y: 738 });
+KEYS.set(',', { x: 666, y: 740 });
+KEYS.set('.', { x: 724, y: 740 });
+KEYS.set('-', { x: 778, y: 740 });
 
 @Component({
     selector: 'app-desk-qs-keyboard',
@@ -160,8 +204,8 @@ export class DeskQsKeyboardComponent {
             x,
             y,
             color: 0x212121,
-            width: 32,
-            height: 32,
+            width: 40,
+            height: 40,
             angle: 0,
             alpha: 0,
         });
